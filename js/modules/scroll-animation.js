12077f92677169cf1d919cdd2c1085c2c2e5e2ef
Aldo-1 <aldo.wanderley@souunit.com.br>
2020-09-02 14:31:47 -0300

    Animaçao ao scroll refeita

--- js/modules/scroll-animation.js
+++ js/modules/scroll-animation.js
@@ -2,27 +2,46 @@ export default class scrollAnima {
   constructor(sections) {
     this.sections = document.querySelectorAll(sections);
     this.metadeAltura = window.innerHeight * 0.6;
-    this.animaScroll = this.animaScroll.bind(this);
+    this.checkDistance = this.checkDistance.bind(this);
   }
-  // Pegando as sessoes
 
-  animaScroll() {
+  // Pegando as distancias do topo das sessoes
+  getDistance() {
+    this.distance = [...this.sections].map((section) => {
+      const sectionTop = section.offsetTop;
+
+      return {
+        element: section,
+        offset: Math.floor(sectionTop - this.metadeAltura),
+      };
+    });
+  }
+
+  // verifica a distancia em cada objeto em relacao ao scrooll do site
+  checkDistance() {
     // Paraca cada sessao(item) ele pega a distancia do topo dela
     // e diminui com o metade altura.
     // Se o section for visivel menor que 0, ele aparece
-    this.sections.forEach((item) => {
-      const sectionTop = item.getBoundingClientRect().top;
-      const isSectionVisible = (sectionTop - this.metadeAltura) < 0;
-      if (isSectionVisible) {
-        item.classList.add('ativo');
-      } else if (item.classList.contains('ativo')) {
-        item.classList.remove('ativo');
+    this.distance.forEach((item) => {
+      if (window.pageYOffset > item.offset) {
+        item.element.classList.add('ativo');
+      } else if (item.element.classList.contains('ativo')) {
+        item.element.classList.remove('ativo');
       }
     });
   }
 
   init() {
-    this.animaScroll();
-    window.addEventListener('scroll', this.animaScroll);
+    if (this.sections.length) {
+      this.getDistance();
+      this.checkDistance();
+      window.addEventListener('scroll', this.checkDistance);
+    }
+    return this;
+  }
+
+  // Remove o evento de scrol
+  stop() {
+    window.removeEventListener('scroll', this.checkDistance);
   }
 }
