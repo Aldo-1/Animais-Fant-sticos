49f129f1ef5329e69f1e43d53d8841dfd6ccf520
Aldo-1 <aldo.wanderley@souunit.com.br>
2020-09-02 15:04:11 -0300

    Refatorando o dropdownmenu

--- js/modules/dropdown.js
+++ js/modules/dropdown.js
@@ -1,17 +1,42 @@
 import outsideClick from './outsideclick.js';
 
-function handleClick(event) {
-  event.preventDefault();
-  this.classList.add('active');
-  outsideClick(this, ['touchstart', 'click'], () => {
-    this.classList.remove('active');
-  });
-}
-export default function initDropdownMenu() {
-  const dropdownMenus = document.querySelectorAll('[data-dropdown]');
-  dropdownMenus.forEach((menu) => {
-    ['touchstart', 'click'].forEach((userEvent) => {
-      menu.addEventListener(userEvent, handleClick);
+export default class DropdownMenu {
+  constructor(dropdownMenus, events) {
+    this.dropdownMenus = document.querySelectorAll(dropdownMenus);
+
+    // define touchstart e click como padrao
+    // se nao for definido
+    if (events === undefined) this.events = ['touchstart', 'click'];
+    else this.events = events;
+
+    this.activeDropDownMenu = this.activeDropDownMenu.bind(this);
+    this.activeClass = 'active';
+  }
+
+  // ativa o  dropdown menu e
+  // ativa a funcao de ousideclick
+  activeDropDownMenu(event) {
+    event.preventDefault();
+    const element = event.currentTarget;
+    element.classList.add(this.activeClass);
+    outsideClick(element, this.events, () => {
+      element.classList.remove(this.activeClass);
     });
-  });
+  }
+
+  // add eventos ao dropdown menu
+  addDropDownMenusEvents() {
+    this.dropdownMenus.forEach((menu) => {
+      this.events.forEach((userEvent) => {
+        menu.addEventListener(userEvent, this.activeDropDownMenu);
+      });
+    });
+  }
+
+  init() {
+    if (this.dropdownMenus.length) {
+      this.addDropDownMenusEvents();
+    }
+    return this;
+  }
 }
